71511c661d42b42d393870d876b0689d71f8abc1
sangeetha <122152646+sangssss@users.noreply.github.com>
2024-11-22 12:39:43 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 5;
+        int num1 = 20;
+        int num2 = 25;
         int sum = add(num1, num2);
         System.out.println("Addition of " + num1 + " and " + num2 + " is: " + sum);
         int difference = subtract(num1, num2);
